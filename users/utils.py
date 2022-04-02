2c8f8fa28a3c9154c149b618ebe48e5ae7777756
SyedKhundmir <syedkhundmir62995@gmail.com>
2022-04-03 00:55:07 +0530

    final commit3

--- users/utils.py
+++ users/utils.py
@@ -151,8 +151,10 @@ def extract_location(locations):
 
 import nltk
 from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
+
 def extract_description(description):
     words = word_tokenize(description)
     processed_words = []
