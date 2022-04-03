d8bf2bd8c9e34b6a46bd6ba2c5275c8c3012b756
SyedKhundmir <syedkhundmir62995@gmail.com>
2022-04-03 16:05:23 +0530

    Committ

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -219,3 +221,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if os.getcwd() == '/app':
     DEBUG = False
+
+
+# Activate django heroku
+django_heroku.settings(locals())
